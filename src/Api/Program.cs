349f54dc0fb8351499fde2acec136b9d78c21cb8
IamAndreek <u20231b120@upc.edu.pe>
2025-12-01 17:39:29 -0500

    feat(develop): railway deployment

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -23,7 +23,7 @@ var configuration = builder.Configuration;
 // Get connection string from configuration (appsettings.json / environment)
 var defaultConn = configuration.GetConnectionString("DefaultConnection")
                   ?? configuration["ConnectionStrings:DefaultConnection"]
-                  ?? "server=localhost;port=3306;database=energix;user=root;password=3xp3ri3nciA*";
+                  ?? "server=localhost;port=3306;database=energix;user=root;password=Password123";
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(defaultConn, ServerVersion.AutoDetect(defaultConn)));
